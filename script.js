6e728cea4a2ac39e037070b971c831519e78c4d1
Faria-Tahmin <thfaria.13@gmail.com>
2020-08-17 14:07:44 +0600

    check lyrics

--- script.js
+++ script.js
@@ -1,35 +1,53 @@
 function getLyrics(artist, title) {
-    fetch(`https://api.lyrics.ovh/v1/${artist}/${title}`)
-        .then(res => res.json())
-        .then(lyricsData => {
-            const lyrics = lyricsData.lyrics;
-            const lyricesDisplay = document.getElementById('single-lyrics');
-            lyricesDisplay.innerHTML = `<h2 class="text-success mb-4">${artist} - ${title}</h2>
-                                        <pre class="lyric text-white">${lyrics}</pre>`
-        })
-        document.getElementById('search-result').innerHTML = ''; // close song suggestions 
+    document.getElementById(`lyrics-button${i + 1}`).addEventListener("click", function () {
+        fetch(`https://api.lyrics.ovh/v1/${songArtist}/${songTitle}`)
+            .then(res => res.json())
+            .then(data => {
+                if (data.lyrics === undefined) {
+                    document.getElementById("heading").innerText = "No lyrics";
+                    document.getElementById("lyrics").innerText = "";
+
+                }
+                else {
+
+                    document.getElementById("heading").innerText = `${songTitle}
+                ${songArtist}`
+                    document.getElementById("lyrics").innerText = data.lyrics;
+                }
+            }
+
+
+    // fetch(`https://api.lyrics.ovh/v1/${artist}/${title}`)
+    //     .then(res => res.json())
+    //     .then(lyricsData => {
+    //         const lyrics = lyricsData.lyrics;
+    //         const lyricesDisplay = document.getElementById('single-lyrics');
+    //         lyricesDisplay.innerHTML = `<h2 class="text-success mb-4">${artist} - ${title}</h2>
+    //                                     <pre class="lyric text-white">${lyrics}</pre>`
+    //     })
+    //     document.getElementById('search-result').innerHTML = ''; // close song suggestions 
+}
 }
 
 
-
 function searchResult() {
-    const songName = document.getElementById('song-name-input').value;
-    document.getElementById('search-result').innerHTML = ''; // remove previously searched songs
-    document.getElementById('single-lyrics').innerHTML = ''; // close previously opened Lyrics
-    fetch(`https://api.lyrics.ovh/suggest/${songName}`)
-    .then(res => res.json())
-    .then(apiData => {
-        const songs = apiData.data;
-        for (let i = 0; i < 10; i++) {
-            const song = songs[i];
-            const title = song.title;
-            const artist = song.artist.name;
-            const type = song.type;
-            const img = song.artist.picture_medium;
-// result template           
+            const songName = document.getElementById('song-name-input').value;
+            document.getElementById('search-result').innerHTML = ''; // remove previously searched songs
+            document.getElementById('single-lyrics').innerHTML = ''; // close previously opened Lyrics
+            fetch(`https://api.lyrics.ovh/suggest/${songName}`)
+                .then(res => res.json())
+                .then(apiData => {
+                    const songs = apiData.data;
+                    for (let i = 0; i < 10; i++) {
+                        const song = songs[i];
+                        const title = song.title;
+                        const artist = song.artist.name;
+                        const type = song.type;
+                        const img = song.artist.picture_medium;
+                        // result template           
 
-            const result = document.getElementById('search-result');
-            result.innerHTML += `<div class="single-result row align-items-center my-3 p-3">
+                        const result = document.getElementById('search-result');
+                        result.innerHTML += `<div class="single-result row align-items-center my-3 p-3">
                                         <div class="col-md-2">
                                         <img class="img-thumbnail" src="${img}" alt="Cover Picture">
                                         </div>
@@ -41,6 +59,6 @@ function searchResult() {
                                         <button onclick="getLyrics('${artist}','${title}')" class="btn btn-success">Get Lyrics</button>
                                     </div>
                                 </div>`
+                    }
+                })
         }
-    })
-}
